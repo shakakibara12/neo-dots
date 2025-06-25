1a7ff19d9a250e975e3e414a271651368330c8c0
shakakibara <aryan.1843@disroot.org>
2025-06-25 13:38:46 +0530

    Change default clipboard behaviour & add orgmode

---
+++ lua/plugins/user.lua
@@ -0,0 +1,31 @@
+-- You can also add or configure plugins by creating files in this `plugins/` folder
+-- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
+-- Here are some examples:
+
+---@type LazySpec
+return {
+
+  "AstroNvim/astrocore",
+  opts = function(_, opts)
+    -- Override the clipboard option
+    opts.options.opt.clipboard = "unnamed" -- Use default neovim mode. 
+  end,
+
+  'nvim-orgmode/orgmode',
+  event = 'VeryLazy',
+  ft = { 'org' },
+  config = function()
+    -- Setup orgmode
+    require('orgmode').setup({
+      org_agenda_files = '~/Documents/notes/**/*',
+      org_default_notes_file = '~/Documents/notes/todo.org',
+    })
+
+    -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+    -- add ~org~ to ignore_install
+    -- require('nvim-treesitter.configs').setup({
+    --   ensure_installed = 'all',
+    --   ignore_install = { 'org' },
+    -- })
+  end,
+}
