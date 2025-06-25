2c20f52573ac07f6c606dc9f3961e6c33026ec73
shakakibara <aryan.1843@disroot.org>
2025-06-25 13:52:49 +0530

    Split orgmode This fixes opts not applying correctly in astrocore.

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -11,21 +11,4 @@ return {
     opts.options.opt.clipboard = "unnamed" -- Use default neovim mode. 
   end,
 
-  'nvim-orgmode/orgmode',
-  event = 'VeryLazy',
-  ft = { 'org' },
-  config = function()
-    -- Setup orgmode
-    require('orgmode').setup({
-      org_agenda_files = '~/Documents/notes/**/*',
-      org_default_notes_file = '~/Documents/notes/todo.org',
-    })
-
-    -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
-    -- add ~org~ to ignore_install
-    -- require('nvim-treesitter.configs').setup({
-    --   ensure_installed = 'all',
-    --   ignore_install = { 'org' },
-    -- })
-  end,
 }
